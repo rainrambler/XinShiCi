5fbe7e36a9097aa3603c65171d09c411e7a8e329
rainrambler <wanganyu@outlook.com>
2022-04-21 11:22:01 +0800

    Update ChinesePoem.go

--- ChinesePoem.go
+++ ChinesePoem.go
@@ -137,10 +137,12 @@ func hasErrorTitle(poem *ChinesePoem) bool {
 	return false
 }
 
+const ErrorChars = `abcdefghijklmnopqrstuvwxyz` +
+	`ABCDEFGHIJKLMNOPQRSTUVWXYZ` + `0123456789`
+
 // English and number chars in Chinese text
 func hasErrorText(text string) bool {
-	return strings.ContainsAny(text, `abcdefghijklmnopqrstuvwxyz`+
-		`ABCDEFGHIJKLMNOPQRSTUVWXYZ`+`0123456789`)
+	return strings.ContainsAny(text, ErrorChars)
 }
 
 // English and number chars in Chinese text
@@ -153,4 +155,41 @@ func findErrorText(poem *ChinesePoem) string {
 	}
 
 	return res
+}
+
+func findAllErrorText(poem *ChinesePoem) ([]string, string) {
+	res := ""
+	arr := []string{}
+	for _, line := range poem.Sentences {
+		subarr := parseAllErrorText(line)
+		if len(subarr) > 0 {
+			arr = append(arr, subarr...)
+			res = line + "#"
+		}
+	}
+
+	return arr, res
+}
+
+func parseAllErrorText(line string) []string {
+	arr := []string{}
+	rs := []rune(line)
+
+	curPart := ""
+	for _, r := range rs {
+		if strings.ContainsRune(ErrorChars, r) {
+			curPart += string(r)
+		} else {
+			if curPart != "" {
+				arr = append(arr, curPart)
+				curPart = ""
+			}
+		}
+	}
+
+	if curPart != "" {
+		arr = append(arr, curPart)
+	}
+
+	return arr
 }
