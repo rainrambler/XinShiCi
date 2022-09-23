a6de81d0eb951f61176cf3ddff2219c94a132d5f
rainrambler <wanganyu@outlook.com>
2022-09-23 16:23:12 +0800

    Create Cipai_test.go

---
+++ Cipai_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMatch2(t *testing.T) {
+	s := "中平中仄仄平平"
+
+	var sen Sentence
+	sen.Parse(s)
+
+	res := sen.Match(`平山栏烂倚晴翁`)
+	expected := true
+
+	if res != expected {
+		t.Errorf("Result: %v, want: %v", res, expected)
+	}
+}
+
+func TestParseString2(t *testing.T) {
+	// 《朝中措·平山堂》
+	s := "中平中仄仄平平，中仄仄平平。中仄中平中仄，中平中仄平平？"
+
+	var cp Cipai
+	res := cp.ParseString(s)
+	cp.DbgPrint()
+
+	expected := true
+
+	if res != expected {
+		t.Errorf("Result: %v, want: %v", res, expected)
+	}
+}
+
+func TestMatch1(t *testing.T) {
+	s := "中平中仄仄平平，中仄仄平平。中仄中平中仄，中平中仄平平？"
+
+	var cp Cipai
+	res := cp.ParseString(s)
+	//cp.DbgPrint()
+
+	expected := true
+
+	if res != expected {
+		t.Errorf("Result: %v, want: %v", res, expected)
+	}
+
+	res = cp.Match(`平山栏槛倚晴翁，山色有无风`)
+	if res != expected {
+		t.Errorf("Result: %v, want: %v", res, expected)
+	}
+}
