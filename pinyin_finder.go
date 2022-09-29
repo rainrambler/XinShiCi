4642073c5fbdaed60294d5981547420ced06217a
rainrambler <wanganyu@outlook.com>
2022-09-29 10:17:03 +0800

    Update pinyin_finder.go

--- pinyin_finder.go
+++ pinyin_finder.go
@@ -7,10 +7,12 @@ import (
 
 type PinyinFinder struct {
 	hz2pinyin map[string]string
+	hz2pingze map[string]int
 }
 
 func (p *PinyinFinder) Init(filename string) {
 	p.hz2pinyin = make(map[string]string)
+	p.hz2pingze = make(map[string]int)
 
 	lines := ReadTxtFile(filename)
 
@@ -20,9 +22,40 @@ func (p *PinyinFinder) Init(filename string) {
 		} else {
 			arr := strings.Split(line, "|")
 
-			p.hz2pinyin[arr[0]] = arr[1]
+			zhchar := arr[0]
+			zhPy := arr[1]
+			p.hz2pinyin[zhchar] = zhPy
+
+			shengdiao := zhPy[len(zhPy)-1:] // last char
+			pingze := GetPingze(shengdiao)
+
+			p.SetPingze(zhchar, pingze)
 		}
 	}
+}
+
+func (p *PinyinFinder) SetPingze(zhchar string, pzval int) {
+	if pzval == PingZeAny {
+		log.Printf("[DBG]Possible error Pingze for %s: %d\n", zhchar, pzval)
+		return
+	}
+
+	if pzval == PingZeUnknown {
+		log.Printf("[DBG]Possible unknown Pingze for %s: %d\n", zhchar, pzval)
+		return
+	}
+
+	v, exists := p.hz2pingze[zhchar]
+	if !exists {
+		p.hz2pingze[zhchar] = pzval
+		return
+	}
+
+	if v == pzval {
+		return
+	}
+
+	p.hz2pingze[zhchar] = PingZeAny
 }
 
 func (p *PinyinFinder) FindPinyin(zhchar string) string {
@@ -31,4 +64,22 @@ func (p *PinyinFinder) FindPinyin(zhchar string) string {
 	}
 
 	return "" // empty
+}
+
+func (p *PinyinFinder) FindPingze(zhchar string) int {
+	if py, ok := p.hz2pinyin[zhchar]; ok {
+		shengdiao := py[len(py)-1:]
+
+		return GetPingze(shengdiao)
+	}
+
+	return PingZeUnknown
+}
+
+func (p *PinyinFinder) FindPingze2(zhchar string) int {
+	if py, ok := p.hz2pingze[zhchar]; ok {
+		return py
+	}
+
+	return PingZeUnknown
 }
