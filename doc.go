c9f74196ceefc3b961b95f27afc81b5aaacf7974
rainrambler <wanganyu@outlook.com>
2022-12-09 18:33:48 +0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -86,6 +86,7 @@ func ReadTxtFile(filename string) []string {
 	return linearr
 }
 
+// Line start with '#' or '%' is comment line
 func IsCommentLine(line string) bool {
 	if strings.HasPrefix(line, "#") {
 		return true
