3778ee5e2ab81cc3fa18a306b04d4dd055e9848a
rainrambler <wanganyu@outlook.com>
2023-05-18 10:33:17 +0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -268,7 +268,7 @@ func SubString(s string, beginPos, size int) string {
 }
 
 // https://stackoverflow.com/questions/18695346/how-to-sort-a-mapstringint-by-its-values
-func printSortedMapByValue(m map[string]int) {
+func PrintSortedMapByValue(m map[string]int) {
 	type kv struct {
 		Key   string
 		Value int
@@ -288,4 +288,35 @@ func printSortedMapByValue(m map[string]int) {
 		fmt.Printf("%s:%d\n", kv.Key, kv.Value)
 	}
 	fmt.Println("<===")
+}
+
+func PrintSortedMapByValueInt(m map[int]int) {
+	type kv struct {
+		Key   int
+		Value int
+	}
+
+	var ss []kv
+	for k, v := range m {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	fmt.Println("===>")
+	for _, kv := range ss {
+		fmt.Printf("%d:%d\n", kv.Key, kv.Value)
+	}
+	fmt.Println("<===")
+}
+
+func PrintMapGroupByValue(m map[string]int) {
+	v2count := make(map[int]int)
+	for _, v := range m {
+		v2count[v] = v2count[v] + 1
+	}
+
+	PrintSortedMapByValueInt(v2count)
 }
