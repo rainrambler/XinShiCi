8243c258179aac9d03003f43981dbbd3f444ebb6
rainrambler <wanganyu@outlook.com>
2023-06-14 10:47:27 +0800

    Update ChinesePoem.go

--- ChinesePoem.go
+++ ChinesePoem.go
@@ -200,6 +200,39 @@ func (p *ChinesePoem) hasRepeatWords() bool {
 	return false
 }
 
+// 花 未 全开月 未 圆 ==> true
+func (p *ChinesePoem) hasRepeatDiffs() bool {
+	if len(p.Sentences) == 0 {
+		return false
+	}
+
+	for _, sentence := range p.Sentences {
+		if HasRepeatCharsZh(sentence) {
+			fmt.Println(sentence)
+			return true
+		}
+	}
+
+	return false
+}
+
+// 花 未 全开月 未 圆
+func (p *ChinesePoem) FindRepeatDiffs2() []string {
+	arr := []string{}
+	if len(p.Sentences) == 0 {
+		return arr
+	}
+
+	for _, sentence := range p.Sentences {
+		if HasRepeatCharsZh(sentence) {
+			//return sentence
+			arr = append(arr, sentence)
+		}
+	}
+
+	return arr
+}
+
 // Not for Chinese string
 func HasRepeatWords(sentense string) bool {
 	totallen := len(sentense)
@@ -236,73 +269,4 @@ func (p *ChinesePoem) Contains(keywords []string) bool {
 	}
 
 	return true
-}
-
-func hasErrorTitle(poem *ChinesePoem) bool {
-	if strings.Contains(poem.Title, `《`) {
-		return true
-	}
-
-	if strings.Contains(poem.Title, `》`) {
-		return true
-	}
-
-	return false
-}
-
-const ErrorChars = `abcdefghijklmnopqrstuvwxyz` +
-	`ABCDEFGHIJKLMNOPQRSTUVWXYZ` + `0123456789`
-
-// English and number chars in Chinese text
-func hasErrorText(text string) bool {
-	return strings.ContainsAny(text, ErrorChars)
-}
-
-// English and number chars in Chinese text
-func findErrorText(poem *ChinesePoem) string {
-	res := ""
-	for _, line := range poem.Sentences {
-		if hasErrorText(line) {
-			res = line + "#"
-		}
-	}
-
-	return res
-}
-
-func findAllErrorText(poem *ChinesePoem) ([]string, string) {
-	res := ""
-	arr := []string{}
-	for _, line := range poem.Sentences {
-		subarr := parseAllErrorText(line)
-		if len(subarr) > 0 {
-			arr = append(arr, subarr...)
-			res = line + "#"
-		}
-	}
-
-	return arr, res
-}
-
-func parseAllErrorText(line string) []string {
-	arr := []string{}
-	rs := []rune(line)
-
-	curPart := ""
-	for _, r := range rs {
-		if strings.ContainsRune(ErrorChars, r) {
-			curPart += string(r)
-		} else {
-			if curPart != "" {
-				arr = append(arr, curPart)
-				curPart = ""
-			}
-		}
-	}
-
-	if curPart != "" {
-		arr = append(arr, curPart)
-	}
-
-	return arr
 }
