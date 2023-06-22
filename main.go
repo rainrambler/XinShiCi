f28b64eb4f922354351fe0ccfb453e8a27fd61b2
rainrambler <wanganyu@outlook.com>
2023-06-22 18:26:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
 func demoText() {
@@ -31,15 +35,122 @@ func readDictFile() {
 	fmt.Printf("Hanzi Count: %d\n", singlecount)
 }
 
+// VerboseMode
+const (
+	Brief    = 2
+	Verbose  = 3
+	VerbMore = 4
+	Unknown  = 1
+)
+
+func findQscByChars() {
+	if len(os.Args) <= 1 {
+		fmt.Printf("Usage: %s --find \"keyword\" --length [d] "+
+			" --pattern [d] [--v] [--vv]\n", os.Args[0])
+		return
+	}
+
+	//fmt.Printf("[DBG]Args: %d\n", len(os.Args))
+
+	var tofind string
+	var length int
+	var verbose bool
+	var verbmore bool
+	var pattern int
+
+	flag.StringVar(&tofind, "find", "", "keyword to find")
+	flag.IntVar(&length, "length", 0, "Sentence length")
+	flag.BoolVar(&verbose, "v", false, "verbose mode")
+	flag.BoolVar(&verbmore, "vv", false, "more verbose mode")
+	flag.IntVar(&pattern, "p", POS_ANY, "match pattern (1: Prefix, 2: Suffix, 3: Any, Other: invalid)")
+
+	flag.Parse()
+
+	log.Printf("[INFO]%s\n", getCommandLine())
+
+	starttime := time.Now()
+
+	verbmode := Brief
+	if verbose {
+		if verbmore {
+			log.Printf("WARN: Invalid parameters: %v and %v.\n", verbose, verbmore)
+			return
+		} else {
+			verbmode = Verbose
+		}
+	} else if verbmore {
+		verbmode = VerbMore
+	}
+
+	findQscKeyword(tofind, pattern, length, verbmode)
+	//FindKeysInDbs(tofind, ignores, inBook, verbmode, pattern)
+
+	elapsed := time.Since(starttime)
+	log.Printf("Looking up took %s.\n", elapsed)
+}
+
+func findRelatedKeyword() {
+	if len(os.Args) == 0 {
+		fmt.Printf("Usage: %s --find \"keyword\" [--v] [--vv]\n", os.Args[0])
+		return
+	}
+
+	var tofind string
+	var verbose bool
+	var verbmore bool
+
+	flag.StringVar(&tofind, "find", "", "keyword to find")
+	flag.BoolVar(&verbose, "v", false, "verbose mode")
+	flag.BoolVar(&verbmore, "vv", false, "more verbose mode")
+
+	flag.Parse()
+
+	log.Printf("[INFO]%s\n", getCommandLine())
+	log.Printf("[INFO]Keyword: %s.\n", tofind)
+
+	starttime := time.Now()
+
+	verbmode := Brief
+	if verbose {
+		if verbmore {
+			log.Printf("WARN: Invalid parameters: %v and %v.\n", verbose, verbmore)
+			return
+		} else {
+			verbmode = Verbose
+		}
+	} else if verbmore {
+		verbmode = VerbMore
+	}
+
+	findRelated(tofind, verbmode)
+	elapsed := time.Since(starttime)
+	log.Printf("Looking up took %s.\n", elapsed)
+}
+
+func getCommandLine() string {
+	s := ""
+	for _, v := range os.Args {
+		s += v + " "
+	}
+	return s
+}
+
 func main() {
-	findKeywords(`夏 茶`)
+	//findKeywords(`端午 客`)
+
+	//findRepeatChChars()
+
+	//findRelated(`黄叶`)
+	//findRelatedKeyword()
+
 	//readQts()
 	//loadPoetryFile(`./quantangshi/005.json`)
 	//loadFileDemo(`./quantangshi/005.json`)
 	//LoadPoetries(`./quantangshi/`)
 	//printHans(`allhans.txt`)
+
 	//analyseQsc()
-	//convertHanzi2Pinyin2()
+	findQscByChars()
 
 	//cipaiDemo()
 
@@ -47,6 +158,7 @@ func main() {
 
 	//ExportQsc(`D:\Projects\GitHubSync\XinShiCi\qsc.txt`, `D:\tmp\xsc`)
 
-	//LoadTxt(`D:\2023\05\Nalan\nalan_all_purified.txt`)
-	//LoadTxt(`D:\Projects\XinShiCi\qsc.txt`)
+	//GenerateWordCloud(`D:\Projects\XinShiCi\qsc.txt`)
+
+	//qscRank()
 }
