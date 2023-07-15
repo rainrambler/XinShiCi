cefa63621ba4ed555118caf4b6b1ab4da6ae66e9
rainrambler <wanganyu@outlook.com>
2023-07-15 19:50:45 +0800

    Update pinyin_finder.go

--- pinyin_finder.go
+++ pinyin_finder.go
@@ -8,29 +8,41 @@ import (
 type PinyinFinder struct {
 	hz2pinyin map[string]string
 	hz2pingze map[string]int
+
+	hz2py map[rune]string
+	hz2pz map[rune]int
 }
 
 func (p *PinyinFinder) Init(filename string) {
 	p.hz2pinyin = make(map[string]string)
 	p.hz2pingze = make(map[string]int)
 
+	p.hz2py = make(map[rune]string)
+	p.hz2pz = make(map[rune]int)
+
 	lines := ReadTxtFile(filename)
 
 	for idx, line := range lines {
 		if !strings.Contains(line, "|") {
-			log.Printf("WARN: Format error in dict line [%d]: %s\n", idx, SubString(line, 0, 10))
-		} else {
-			arr := strings.Split(line, "|")
-
-			zhchar := arr[0]
-			zhPy := arr[1]
-			p.hz2pinyin[zhchar] = zhPy
-
-			shengdiao := zhPy[len(zhPy)-1:] // last char
-			pingze := GetPingze(shengdiao)
-
-			p.SetPingze(zhchar, pingze)
+			log.Printf("WARN: Format error in dict line [%d]: %s\n",
+				idx, SubString(line, 0, 10))
+			continue
 		}
+
+		arr := strings.Split(line, "|")
+
+		zhchar := arr[0]
+		zhPy := arr[1]
+		p.hz2pinyin[zhchar] = zhPy
+
+		zhr := GetFirstRune(zhchar)
+		p.hz2py[zhr] = zhPy
+
+		shengdiao := zhPy[len(zhPy)-1:] // last char
+		pingze := GetPingze(shengdiao)
+
+		p.SetPingze(zhchar, pingze)
+		p.SetPingze2(zhr, pingze)
 	}
 }
 
@@ -56,6 +68,30 @@ func (p *PinyinFinder) SetPingze(zhchar string, pzval int) {
 	}
 
 	p.hz2pingze[zhchar] = PingZeAny
+}
+
+func (p *PinyinFinder) SetPingze2(zhchar rune, pzval int) {
+	if pzval == PingZeAny {
+		log.Printf("[DBG]Possible error Pingze for %s: %d\n", zhchar, pzval)
+		return
+	}
+
+	if pzval == PingZeUnknown {
+		log.Printf("[DBG]Possible unknown Pingze for %s\n", zhchar)
+		return
+	}
+
+	v, exists := p.hz2pz[zhchar]
+	if !exists {
+		p.hz2pz[zhchar] = pzval
+		return
+	}
+
+	if v == pzval {
+		return
+	}
+
+	p.hz2pz[zhchar] = PingZeAny
 }
 
 func (p *PinyinFinder) FindPinyin(zhchar string) string {
