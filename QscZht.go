d664c44c14b785c6686f46d6b4b88cdf6bc1f403
rainrambler <wanganyu@outlook.com>
2023-07-22 11:10:50 +0800

    Update QscZht.go

--- QscZht.go
+++ QscZht.go
@@ -218,6 +218,10 @@ func isSequenceCipai(cipainame string) bool {
 		return true
 	}
 
+	if cipainame == `同上` {
+		return true
+	}
+
 	arr := []string{`一`, `二`, `三`, `四`, `五`, `六`, `七`, `八`, `九`, `十`, `十一`,
 		`十二`, `十三`, `十四`, `十五`}
 	for _, item := range arr {
