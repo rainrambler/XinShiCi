f71af9d6277eb3dfa4a182a2d75dde12adaa96e6
rainrambler <wanganyu@outlook.com>
2023-08-08 09:28:25 +0800

    Update Poets.go

--- Poets.go
+++ Poets.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -48,4 +49,52 @@ func getPoetName(line string) string {
 	}
 
 	return strings.TrimSpace(line[:pos])
+}
+
+type AncientPoets struct {
+	poet2id map[string]int
+}
+
+func (p *AncientPoets) Init(filename string) {
+	p.poet2id = make(map[string]int)
+
+	lines := ReadTxtFile(filename)
+
+	id := 100
+	for _, line := range lines {
+		if len(line) > 0 {
+			poetname := parsePoetName(line)
+			p.poet2id[poetname] = id + 2
+		}
+	}
+}
+
+func (p *AncientPoets) IsPoet(nm string) bool {
+	if _, ok := p.poet2id[nm]; ok {
+		return true
+	}
+	return false
+}
+
+func (p *AncientPoets) FindPoet(nm string) int {
+	if id, ok := p.poet2id[nm]; ok {
+		return id
+	}
+	return -1
+}
+
+func (p *AncientPoets) Count() int {
+	return len(p.poet2id)
+}
+
+// 宋·蔡確 // 宋史 卷211
+func parsePoetName(line string) string {
+	dynAuthor := getPoetName(line)
+	_, author := SplitZhString(dynAuthor, '·')
+	if author == "" {
+		fmt.Printf("Cannot parse %s\n", line)
+		return ""
+	}
+
+	return author
 }
