6732b5323bfc7865a04e889d1c9d06303062cd47
rainrambler <wanganyu@outlook.com>
2024-09-06 17:06:47 +0800

    Update QscConvertor_test.go

--- QscConvertor_test.go
+++ QscConvertor_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 )
 
@@ -35,121 +33,6 @@ func TestHasCipai2(t *testing.T) {
 	s = `春事阑珊芳草歇。`
 	if hascipai, _ := allCipai.HasCipai(s); hascipai {
 		t.Errorf("TestHasCipai2 failed: %v", s)
-	}
-}
-
-func TestConvertLines(t *testing.T) {
-	s := `
-	
-	苏轼
-	临江仙
-	紫曲迷香，绿窗梦月。
-	紫曲迷香，绿窗梦月。
-	卜算子
-	紫曲迷香，绿窗梦月。
-	紫曲迷香，绿窗梦月。
-	
-	晏几道
-	西江月
-	紫曲迷香，绿窗梦月。
-	紫曲迷香，绿窗梦月。
-	
-	`
-
-	lines := strings.Split(s, "\n")
-	var qc QscConv
-	qc.Init()
-
-	qc.convertLines(lines, false)
-
-	for _, poem := range qc.allPoems.ID2Poems {
-		fmt.Println(poem.toDesc())
-	}
-
-	actCount := qc.allPoems.Count()
-	if actCount != 3 {
-		t.Errorf("TestConvertLines failed: %v", actCount)
-	}
-}
-
-func TestConvertLines2(t *testing.T) {
-	s := `
-	
-苏轼
-满庭芳
-香叆雕盘，寒生冰箸，画堂别是风光。主人情重，开宴出红妆。腻玉圆搓素颈，藕丝嫩、新织仙裳。双歌罢，虚檐转月，余韵尚悠扬。 
-人间，何处有，司空见惯，应谓寻常。坐中有狂客，恼乱愁肠。报道金钗坠也，十指露、春笋纤长。亲曾见，全胜宋玉，想像赋高唐。 
-满庭芳
-蜗角虚名，蝇头微利，算来著甚干忙。事皆前定，谁弱又谁强。且趁闲身未老，尽放我、些子疏狂。百年里，浑教是醉，三万六千场。 
-思量。能几许，忧愁风雨，一半相妨，又何须，抵死说短论长。幸对清风皓月，苔茵展、云幕高张。江南好，千钟美酒，一曲满庭芳。 
-
-	`
-
-	lines := strings.Split(s, "\n")
-
-	var qc QscConv
-	qc.Init()
-
-	qc.convertLines(lines, false)
-
-	actCount := qc.allPoems.Count()
-	if actCount != 2 {
-		t.Errorf("TestConvertLines2 failed: %v", actCount)
-	}
-
-	ids := qc.allPoems.GetAllIDs()
-
-	poem := qc.allPoems.GetPoem(ids[0])
-
-	sentencesize := len(poem.Sentences)
-
-	if sentencesize != 23 {
-		t.Errorf("TestConvertLines2 failed: want 23, actual %v", sentencesize)
-	}
-
-	for _, sentence := range poem.Sentences {
-		fmt.Printf("[%s]\n", sentence)
-	}
-}
-
-func TestConvertLines3(t *testing.T) {
-	s := `
-	
-姜个翁
-霓裳中序第一（春晚旅寓）
-园林罢组织。树树东风翠云滴。草满旧家行迹。
-龟石。当年第一。也似老、人间风日。
-
-	`
-
-	lines := strings.Split(s, "\n")
-
-	var qc QscConv
-	qc.Init()
-
-	qc.convertLines(lines, false)
-
-	actCount := qc.allPoems.Count()
-	if actCount != 1 {
-		t.Errorf("TestConvertLines3 failed: %v", actCount)
-	}
-
-	for _, poem := range qc.allPoems.ID2Poems {
-		fmt.Println(poem.toDesc())
-	}
-
-	ids := qc.allPoems.GetAllIDs()
-
-	poem := qc.allPoems.GetPoem(ids[0])
-
-	sentencesize := len(poem.Sentences)
-
-	if sentencesize != 7 {
-		t.Errorf("TestConvertLines3 failed: want 7, actual %v", sentencesize)
-	}
-
-	for _, sentence := range poem.Sentences {
-		fmt.Printf("[%s]\n", sentence)
 	}
 }
 
