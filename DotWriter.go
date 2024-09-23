813fc72230e041147b5b4eedd8b64a3d6ee34f1b
rainrambler <wanganyu@outlook.com>
2024-09-23 23:31:26 +0800

    Update DotWriter.go

--- DotWriter.go
+++ DotWriter.go
@@ -38,6 +38,7 @@ func (p *DotFile) AddNode(oneWord string) {
 	}
 
 	p.Nodes[oneWord] = 1
+	//fmt.Printf("[DBG]Node added: %s, Total: %d\n", oneWord, len(p.Nodes))
 }
 
 func (p *DotFile) AddLink(word1 string, word2 string, cnt int) {
@@ -74,18 +75,14 @@ func (p *DotFile) getLinkCount(word1 string, word2 string) int {
 	return v
 }
 
-const TOP_WORD = 10
+const TOP_WORD = 8
 
 func (p *DotFile) Generate(filename string) {
 	lines := []string{}
 
-	header := `
-	digraph ChWordLinks {
+	header := `digraph ChWordLinks {
 	fontname = "SimHei";
-	graph [	fontname = "SimHei",
-		fontsize = 36,
-		label = "\n\n\n\nObject Oriented Graphs\nStephen North, 3/19/93",
-		];
+
 	node [	shape = polygon,
 		fontname="SimHei,SimSun"
 		sides = 4,
@@ -106,6 +103,50 @@ func (p *DotFile) Generate(filename string) {
 		if total >= TOP_WORD {
 			break
 		}
+	}
+
+	for _, entry := range p.Word2Word.Entries() {
+		wd1 := entry.Key.(string)
+		wd2 := entry.Value.(string)
+
+		cnt := p.getLinkCount(wd1, wd2)
+
+		if cnt == 0 {
+			fmt.Printf("[INFO]Pair not found: %s and %s!\n", wd1, wd2)
+		} else {
+			line := fmt.Sprintf(`"%s" -> "%s" [label="%d" color="coral1"];`,
+				entry.Key.(string), entry.Value.(string), cnt)
+			lines = append(lines, line)
+		}
+	}
+
+	footer := `}`
+	lines = append(lines, footer)
+
+	WriteLines(lines, filename)
+}
+func (p *DotFile) GenerateFull(filename string) {
+	lines := []string{}
+
+	header := `
+	digraph ChWordLinks {
+	fontname = "SimHei";
+
+	node [	shape = polygon,
+		fontname="SimHei,SimSun"
+		sides = 4,
+		distortion = "0.0",
+		orientation = "0.0",
+		skew = "0.0",
+		color = Gray,
+		style = filled];
+	`
+
+	lines = append(lines, header)
+
+	for node, _ := range p.Nodes {
+		line := node + " " + `[sides=6, distortion="0.936354", orientation=28, skew="-0.126818", color=salmon2];`
+		lines = append(lines, line)
 	}
 
 	for _, entry := range p.Word2Word.Entries() {
