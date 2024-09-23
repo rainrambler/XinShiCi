53cda7bc5395bc5d7224264bb7918ed994505380
rainrambler <wanganyu@outlook.com>
2024-09-23 23:31:38 +0800

    Update HtmlStriper.go

--- HtmlStriper.go
+++ HtmlStriper.go
@@ -3,40 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/grokify/html-strip-tags-go" // => strip
 	"github.com/microcosm-cc/bluemonday"
 )
-
-type HtmlStriper struct {
-	basedir string
-
-	allLines []string
-}
-
-func (p *HtmlStriper) ParseDir(dirname string) {
-	p.basedir = dirname
-	p.allLines = []string{}
-
-	files := FindAllFilesInDir(dirname)
-	for _, onefile := range files {
-		p.ParseFile(onefile)
-	}
-}
-
-// clean all html tags
-func (p *HtmlStriper) ParseFile(filename string) {
-	//fmt.Printf("Parsing %s...\n", filename)
-	// create from a file
-	s, err := ReadTextFile(filename)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	stripped := strip.StripTags(s)
-	p.allLines = append(p.allLines, stripped)
-	//WriteTextFile(filename+".txt", stripped)
-}
 
 type HtmlStriper2 struct {
 	basedir string
